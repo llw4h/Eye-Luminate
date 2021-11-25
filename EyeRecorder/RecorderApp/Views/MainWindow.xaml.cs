927d206bd9edeb3b87922dad4bd8cca90d16f349
llw4h <30249831+llw4h@users.noreply.github.com>
2021-11-25 16:40:43 +0800

    tested new title bar

--- EyeRecorder/RecorderApp/Views/MainWindow.xaml.cs
+++ EyeRecorder/RecorderApp/Views/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Win32;
-using RecorderApp.ViewModels;
-using System;
+﻿using RecorderApp.ViewModels;
 using System.ComponentModel;
 using System.Windows;
+using System;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
 
 namespace RecorderApp.Views
 {
@@ -11,14 +14,138 @@ namespace RecorderApp.Views
     /// </summary>
     public partial class MainWindow : Window, MainView
     {
-        
-        public MainWindow()
+        //
+        #region ResizeWindows
+        bool ResizeInProcess = false;
+        private void Resize_Init(object sender, MouseButtonEventArgs e)
         {
-            InitializeComponent(); 
-
-            Loaded += MainWindow_Loaded;
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                ResizeInProcess = true;
+                senderRect.CaptureMouse();
+            }
         }
 
+        private void Resize_End(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                ResizeInProcess = false; ;
+                senderRect.ReleaseMouseCapture();
+            }
+        }
+
+        private void Resizeing_Form(object sender, MouseEventArgs e)
+        {
+            if (ResizeInProcess)
+            {
+                Rectangle senderRect = sender as Rectangle;
+                Window mainWindow = senderRect.Tag as Window;
+                if (senderRect != null)
+                {
+                    double width = e.GetPosition(mainWindow).X;
+                    double height = e.GetPosition(mainWindow).Y;
+                    senderRect.CaptureMouse();
+                    if (senderRect.Name.ToLower().Contains("right"))
+                    {
+                        width += 5;
+                        if (width > 0)
+                            mainWindow.Width = width;
+                    }
+                    if (senderRect.Name.ToLower().Contains("left"))
+                    {
+                        width -= 5;
+                        mainWindow.Left += width;
+                        width = mainWindow.Width - width;
+                        if (width > 0)
+                        {
+                            mainWindow.Width = width;
+                        }
+                    }
+                    if (senderRect.Name.ToLower().Contains("bottom"))
+                    {
+                        height += 5;
+                        if (height > 0)
+                            mainWindow.Height = height;
+                    }
+                    if (senderRect.Name.ToLower().Contains("top"))
+                    {
+                        height -= 5;
+                        mainWindow.Top += height;
+                        height = mainWindow.Height - height;
+                        if (height > 0)
+                        {
+                            mainWindow.Height = height;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+        #region TitleButtons
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeClick(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (e.ClickCount == 2)
+                {
+                    AdjustWindowSize();
+                }
+                else
+                {
+                    App.Current.MainWindow.DragMove();
+                }
+            }
+        }
+
+        private void AdjustWindowSize()
+        {
+            if (App.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                App.Current.MainWindow.WindowState = WindowState.Normal;
+                MaximizeButton.Content = "";
+            }
+            else if (App.Current.MainWindow.WindowState == WindowState.Normal)
+            {
+                App.Current.MainWindow.WindowState = WindowState.Maximized;
+                MaximizeButton.Content = "";
+            }
+        }
+
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+        #endregion
+        //
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+
+            //limit for height to not overlay system taskbar
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            HeaderIcon.Source = BitmapFrame.Create(new Uri("pack://application:,,,/favicon.ico", UriKind.RelativeOrAbsolute));
+            HeaderTitle.Text = "Eye Luminate";
+        }
+        
+      
         
 
         /// <summary>
